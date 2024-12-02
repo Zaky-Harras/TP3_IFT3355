3e52c88a018fea5647e9fef167fa8f50489d47c5
Zaky Harras <zaky.harras@umontreal.ca>
2024-12-02 12:49:52 -0500

    Première version d'animation. Amélioration à venir.

--- TP3_Code/TP3_Physics.js
+++ TP3_Code/TP3_Physics.js
@@ -34,7 +34,7 @@ TP3.Physics = {
 		return mass;
 	},
 
-	applyForces: function (node, dt, time) {
+	applyForces: async function (node, dt, time) {
 
 		var u = Math.sin(1 * time) * 4;
 		u += Math.sin(2.5 * time) * 2;
@@ -50,9 +50,36 @@ TP3.Physics = {
 		node.vel.add(new THREE.Vector3(0, -node.mass, 0).multiplyScalar(dt));
 
 		// TODO: Projection du mouvement, force de restitution et amortissement de la velocite
+		await new Promise(r => setTimeout(r, 10));
+		if(node.parentRotation){
+			node.p0.applyMatrix4(node.parentRotation);
+			p1_t_plus_dt = node.p1.clone().add(node.vel.clone().multiplyScalar(dt/100000));			//p1_t_plus_dt = p1_t + v * dt
+			newVector = p1_t_plus_dt.clone().add(node.p0.clone().multiplyScalar(-1));				//newVector = p1_t_plus_dt - p0
+			newVector.normalize();
+			oldVector = node.p1.clone().add(node.p0.clone().multiplyScalar(-1));					//oldVector = p1_t - p0
+			oldVector.normalize();
+			rotationAngle = oldVector.angleTo(newVector);
+			rotationMatrix = new THREE.Matrix4().makeRotationAxis(oldVector.clone().cross(newVector).normalize(), rotationAngle);	//Trouver matrix de rotation
+			tranformationMatrix = rotationMatrix.multiply(node.parentRotation);
+		} else {
+			rotationMatrix = new THREE.Matrix4();
+			tranformationMatrix = rotationMatrix;
+		}
+		old_p1 = node.p1;
+		node.p1.applyMatrix4(tranformationMatrix); 													//Appliquer matrix de rotation à p1_t
+		node.vel = old_p1.clone().add(node.p1.clone().addScalar(-1));							    //Remplacer l'ancienne velocite par la vrai velocite
+
+		velRestitution = node.vel.clone().multiplyScalar(-0.5);
+		velRestitution.x = velRestitution.x^2;
+		velRestitution.y = velRestitution.y^2;
+		velRestitution.z = velRestitution.z^2;
+		velRestitution.multiplyScalar(node.a0*1000);
+		node.vel.add(velRestitution);
+		node.vel.multiplyScalar(0.7);
 
 		// Appel recursif sur les enfants
 		for (var i = 0; i < node.childNode.length; i++) {
+			node.childNode[i].parentRotation = tranformationMatrix;
 			this.applyForces(node.childNode[i], dt, time);
 		}
 	}
